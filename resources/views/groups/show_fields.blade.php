4969dc2e708edcd96b072b52c9460a4bff67366d
NurbakiT <nurbakit_5496@mail.ru>
2020-03-22 18:22:10 +0600

    rating

--- resources/views/groups/show_fields.blade.php
+++ resources/views/groups/show_fields.blade.php
@@ -1,7 +1,7 @@
 <!-- Department Id Field -->
 <div class="form-group">
     {!! Form::label('department_id', 'Кафедра:') !!}
-    <p>{{ $group->departments->title }}</p>
+    <p>{{ isset($group->departments->title) ? $group->departments->title : 'Не указано'}}</p>
 </div>
 
 <!-- Title Field -->
@@ -13,12 +13,12 @@
 <!-- Language Id Field -->
 <div class="form-group">
     {!! Form::label('language_id', 'Язык обучения:') !!}
-    <p>{{ $group->languages->title }}</p>
+    <p>{{ isset($group->languages->title) ? $group->languages->title : 'Не указано'}}</p>
 </div>
 
 <!-- Education Id Field -->
 <div class="form-group">
     {!! Form::label('education_id', 'Тип обучения:') !!}
-    <p>{{ $group->educations->title }}</p>
+    <p>{{ isset($group->educations->title) ? $group->educations->title : 'Не указано' }}</p>
 </div>
 
